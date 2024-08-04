e6f1a0c145702a51b694c659a8081be6c9c6ecba
system64fumo <system64fumo@protonmail.com>
2024-08-04 20:32:51 +0300

    Add shift labels
    Pain.

--- src/key.cpp
+++ src/key.cpp
@@ -1,5 +1,15 @@
 #include "key.hpp"
 
 key::key(const int &code, const std::string &label, const std::string &label_shift) {
-	set_text(label);
+	this->code = code;
+	this->label = label;
+	this->label_shift = label_shift;
+	set_shift(false);
+}
+
+void key::set_shift(const bool &state) {
+	if (state)
+		set_text(label_shift);
+	else
+		set_text(label);
 }
