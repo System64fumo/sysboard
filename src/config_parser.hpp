b24e8308a48d1d1c8e33f2fdf0e41152e3bbde0a
system64fumo <system64fumo@tuta.io>
2024-08-13 19:36:45 +0300

    Add config file support
    Load config via config.conf
    Whoops forgot to add this!

---
+++ src/config_parser.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include <map>
+
+// INI parser
+class config_parser {
+	public:
+		config_parser(const std::string &filename);
+		std::string get_value(const std::string &section, const std::string &key);
+		bool available;
+
+	private:
+		std::map<std::string, std::map<std::string, std::string>> data;
+		std::string trim(const std::string &str);
+};
