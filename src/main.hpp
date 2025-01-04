ece4c82b123867a2c01ab974106e6e7391e93309
system64fumo <system64fumo@tuta.io>
2025-01-05 00:21:48 +0200

    Update config system
    Cleanup is sorta needed to be honest.

--- src/main.hpp
+++ src/main.hpp
@@ -1,12 +1,11 @@
 #pragma once
-#include "config.hpp"
+#include <map>
+#include <string>
 
 class sysboard;
 sysboard* window;
 
-config_board config_main;
-
-typedef sysboard* (*sysboard_create_func)(const config_board&);
+typedef sysboard* (*sysboard_create_func)(const std::map<std::string, std::map<std::string, std::string>>&);
 sysboard_create_func sysboard_create_ptr;
 
 typedef void (*sysboard_handle_signal_func)(sysboard*, int);
